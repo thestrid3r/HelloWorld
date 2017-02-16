8f68ea2e94e01c3893f352d2606724e734c76371
ankit <ankeetsinha@yahoo.com>
2017-02-16 14:48:10 +0000

    port

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ redis = Redis(host='redis', port=6379)
 def hello():
     redis.incr('hits')
     return 'Hello World! I have been seen %s time and my hostname is %s .' %(redis.get('hits'),socket.gethostname())
-#           "Your current server is " + socket.gethostname()
 #@app.route('/host')
 #def host():
 #	return "Your current server is " + socket.gethostname()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=8080)
